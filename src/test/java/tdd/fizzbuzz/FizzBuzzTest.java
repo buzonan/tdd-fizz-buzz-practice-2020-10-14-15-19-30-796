790ad470f1aebbc9d745ff1868cbd2f6d27a106d
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-15 14:00:16 +0800

    [FizzBuzz][andrey]test: create unit test for FizzBuzz to output Fizz when given 3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_Fizz_when_number_modulo_3_given_3() {
+        //Given
+        int orderNumber = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //When
+        String actual = fizzBuzz.countOff(orderNumber);
+
+        //Then
+        assertEquals("Fizz", actual);
+    }
 }
