9c745ac5c7cd8949e889ddd935de061875ddc9a3
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-15 14:18:18 +0800

    [FizzBuzz][andrey]refactor: implement constant variables for special numbers

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,15 +5,18 @@ public class FizzBuzz {
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
     public static final String WHIZZ = "Whizz";
+    public static final int FIZZ_SPECIAL_NUMBER = 3;
+    public static final int BUZZ_SPECIAL_NUMBER = 5;
+    public static final int WHIZZ_SPECIAL_NUMBER = 7;
 
     public String countOff(int orderNumber) {
-        if(isModuloOf(orderNumber, 3)){
+        if(isModuloOf(orderNumber, FIZZ_SPECIAL_NUMBER)){
             return FIZZ;
         }
-        if(isModuloOf(orderNumber, 5)){
+        if(isModuloOf(orderNumber, BUZZ_SPECIAL_NUMBER)){
             return BUZZ;
         }
-        if(isModuloOf(orderNumber, 7)){
+        if(isModuloOf(orderNumber, WHIZZ_SPECIAL_NUMBER)){
             return WHIZZ;
         }
         return String.valueOf(orderNumber);
